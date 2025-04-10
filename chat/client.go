afbb9e8bb9d4f179cc788b609cfa7b04beb8ff3b
Jun <junjie.lai@sjsu.edu>
2025-04-09 20:37:29 -0700

    Minor updates

--- chat/client.go
+++ chat/client.go
@@ -2,7 +2,6 @@ package chat
 
 import (
 	"context"
-	"fmt"
 	"github.com/JunJie-Lai/Chat-App/internal/data"
 	"github.com/coder/websocket"
 	"github.com/coder/websocket/wsjson"
@@ -28,9 +27,8 @@ type Message struct {
 
 func (client *Client) ReadMessage() {
 	defer func(Conn *websocket.Conn) {
-		fmt.Println("READ CLOSE")
 		client.Server.Unregister <- client
-		if err := Conn.Close(websocket.StatusNormalClosure, client.User.Name+" disconnected."); err != nil {
+		if err := Conn.Close(websocket.StatusNormalClosure, "Disconnected."); err != nil {
 			return
 		}
 	}(client.Conn)
@@ -39,7 +37,7 @@ func (client *Client) ReadMessage() {
 		_, message, err := client.Conn.Read(context.Background())
 		if err != nil {
 			if websocket.CloseStatus(err) != websocket.StatusGoingAway {
-				log.Println(3, err)
+				log.Println("Read error: ", err)
 			}
 			break
 		}
@@ -53,24 +51,25 @@ func (client *Client) ReadMessage() {
 }
 
 func (client *Client) WriteMessage() {
-	defer func(client *Client) {
-		fmt.Println("WRITE CLOSE")
-		if err := client.Conn.Close(websocket.StatusNormalClosure, client.User.Name+" disconnected."); err != nil {
+	defer func(Conn *websocket.Conn) {
+		if err := Conn.Close(websocket.StatusNormalClosure, "Disconnected."); err != nil {
 			return
 		}
-	}(client)
+	}(client.Conn)
 
 	for {
 		if err := wsjson.Write(context.Background(), client.Conn, <-client.Message); err != nil {
 			if websocket.CloseStatus(err) != -1 {
-				log.Println(4, err)
+				log.Println("Write error: ", err)
 			}
 			break
 		}
 
 		for msg := range client.Message {
 			if err := wsjson.Write(context.Background(), client.Conn, msg); err != nil {
-				log.Println(4.5, err)
+				if websocket.CloseStatus(err) != -1 {
+					log.Println("Write message history error: ", err)
+				}
 				break
 			}
 		}
